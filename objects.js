8c832a6ff490a922293bee8afeac69025f59ee14
Margarita D <margaritdrozdoff@gmail.com>
2018-01-29 11:05:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,12 @@
 var playlist = {artist:'joan baez', song: 'Diamonds and Rust'}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
+}
